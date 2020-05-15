dcc2171bb1084814e74e437a89794fd1ce339401
melkon_2010@outlook.com <melkon_2010@outlook.com>
2020-05-15 10:54:15 +0200

    Added getMsg function

--- src/websocket/ChatServer.java
+++ src/websocket/ChatServer.java
@@ -1,55 +1,56 @@
 package websocket;
 
-import com.google.gson.Gson;
-
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 @ServerEndpoint("/ws")
 public class ChatServer {
-    private Map<String, String> usernames = new HashMap<String, String>();
 
-    @OnOpen
-    public void open(Session session) throws IOException, EncodeException {
-
-
+    public  static String  getMsg() throws MalformedURLException {
+        String res,msg,temp,time;
         URL u = new URL("https://mdna.azurewebsites.net/api/GetDataFromTableStorage?limitRows=1&orderBy=desc");
+
         try (InputStream in = u.openStream()) {
             Object obj = new String(in.readAllBytes());
-            session.getBasicRemote().sendText("Senaste meddelandet."+ obj.toString() );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            msg = obj.toString();
+
+            temp = msg.substring(3,21);
+            StringBuilder sb = new StringBuilder(temp);
+            sb.deleteCharAt(11);
+            temp= sb.toString();
+
+            time = msg.substring(91,122);
+            StringBuilder sb1 = new StringBuilder(time);
+            sb1.deleteCharAt(9);
+            sb1.deleteCharAt(10);
+            time= sb1.toString();
+
+            res =temp+time;
+            return res;
+
+        } catch (IOException e) { e.printStackTrace(); }
+        return  "";
+    }
+    @OnOpen
+    public void open(Session session) throws IOException {
+            session.getBasicRemote().sendText("Senaste meddelandet."+getMsg());
     }
 
     @OnClose
-    public void close(Session session) throws IOException, EncodeException {
+    public void close(Session session){
     }
 
     @OnMessage
     public void handleMessage(String message, Session session) throws IOException, EncodeException, InterruptedException {
-        String userId = session.getId();
-        while (!usernames.containsValue(message))
+        while (true)
         {
-            String username = usernames.get(userId);
-            for (Session peer : session.getOpenSessions())
-            {
-                URL u = new URL("https://mdna.azurewebsites.net/api/GetDataFromTableStorage?limitRows=1&orderBy=desc");
-
-                try (InputStream in = u.openStream())
-                {
-                     Object obj = new String(in.readAllBytes());
-                    peer.getBasicRemote().sendText("Senaste meddelandet."+ obj.toString());
+            for (Session peer : session.getOpenSessions()) {
+                    peer.getBasicRemote().sendText("Senaste meddelandet."+ getMsg());
                     Thread.sleep(7000);
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
